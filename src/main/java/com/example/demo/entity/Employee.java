38b3333a2bf3b16390d8846ce90071492640e20c
William Polinchak <polinchw@gmail.com>
2022-12-20 11:06:45 -0500

    fix: Add Entity class.

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+  
+// @Entity annotation defines that a 
+// class can be mapped to a table
+@Entity 
+public class Employee {
+    
+    // @ID This annotation specifies 
+    // the primary key of the entity.
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private long id;
+    private String name;
+    private String city;
+  
+    public Employee() {
+        super();
+    }
+    public Employee(String name, String city) {
+        super();
+        this.name = name;
+        this.city = city;
+    }
+  
+    public String getName() {
+        return name;
+    }
+  
+    public void setName(String name) {
+        this.name = name;
+    }
+  
+    public String getCity() {
+        return city;
+    }
+  
+    public void setCity(String city) {
+        this.city = city;
+    }
+  
+}
