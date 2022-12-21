2077667ace4127c7f60269ec70ad33c9bf3e029c
William Polinchak <polinchw@gmail.com>
2022-12-21 14:04:18 -0500

    fix: The saveEmployee API is working.  For some reason the test is not.

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
 import com.example.demo.rest.repository.EmployeeRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api")
@@ -28,12 +31,10 @@ public class EmployeeController {
         return employee;
     }
 
-    @PostMapping("/saveemployee/{name}/{city}")
-    public void saveEmployee(@PathVariable String name, @PathVariable String city) {
-        log.info("Saving employee %s",name);
-        Employee employee = new Employee();
-        employee.setName(name);
-        employee.setCity(city);
+    @RequestMapping(value = "/saveEmployee/", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity < String > saveEmployee(@RequestBody Employee employee) {
+        log.info("Saving employee {}",employee.getName());
         employeeRepository.save(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/test/java/com/example/demo/DemoApplicationMVCTests.java
+++ src/test/java/com/example/demo/DemoApplicationMVCTests.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.example.demo.controller.EmployeeController;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.example.demo.model.Employee;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
@@ -30,4 +32,23 @@ class DemoApplicationMVCTests {
 		.andExpect(status().isNotFound());
 	}
 
+	@Test
+	public void createEmployeeAPI() throws Exception {
+		mvc.perform( MockMvcRequestBuilders
+				.post("/api/saveEmployee")
+				.content(asJsonString(new Employee("Bill", "Venice")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+	}
+
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
